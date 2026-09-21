5663f2e8ee6c9ee1352e3e1eb1edc453cda40f7b
Gabriel TA <bielldt3@gmail.com>
2026-09-20 21:27:18 -0300

    navegacaoentre telas funcionando, cadastro de eventos nao funcionando, vizualizacao e pesuisa de eventos cadastrados nao funcionando, toda a parte de conexao entre dashboard e eventos nao funcionando

--- js/app.js
+++ js/app.js
@@ -1,84 +1,200 @@
-//popstate listener detecta quando o usuário usa o botão "Voltar" e carrega o conteúdo correto.
-window.addEventListener("popstate", function(event) {
-    if (event.state && event.state.page) {
-        loadPage(event.state.page, false); // Carrega a página sem adicionar novo estado ao histórico
-    }
-});
+//==================================navegacao==================================\\
+//Ler o valor de data-view usando dataset.
+const btnDashboard = document.querySelector('[data-view="dashboard"]');
+const btnCadEvento = document.querySelector('[data-view="novo"]');
+const btnCheckEvnt = document.querySelector('[data-view="eventos"]');
 
-const barraNavegacao = document.getElementById(".navbar-nav");
+const sDash = document.getElementsByClassName("view-dashboard");
+const sNovo = document.getElementsByClassName("view-novo");
+const sEvnt = document.getElementsByClassName("view-eventos");
 
-const item = document.querySelectorAll(".navbar-nar .nav-link");
-    
-barraNavegacao.addEventListener("click",function(){
+//Escutar o evento click.
+btnDashboard.addEventListener("click",function(impede){
+    //Impedir o comportamento padrão do link.
+    impede.preventDefault();
 
-    // Remove o active de todos os botões
-    document.querySelectorAll(".navbar-nav .nav-link")
-        .forEach(link => {
-            link.classList.remove("active");
-            link.removeAttribute("aria-current");
-        });
+    btnDashboard.classList.add("active");
+    btnCadEvento.classList.remove("active");
+    btnCheckEvnt.classList.remove("active");
 
-    // Procura o botão correspondente à página
-    const activeLink = document.querySelector(
-        `.navbar-nav .nav-link[data-page="${page}"]`
-    );
-
-    // Coloca o active nele
-    if (activeLink) {
-        activeLink.classList.add("active");
-        activeLink.setAttribute("aria-current", "page");
-    }
-    
-    if (addHistory) {
-        history.pushState({ page: page }, "", "#" + page);
-    }
+    sDash[0].classList.remove("d-none");
+    sNovo[0].classList.add("d-none");
+    sEvnt[0].classList.add("d-none");
 })
-    
 
-// Define um estado inicial no carregamento da página
-document.addEventListener("DOMContentLoaded", function () {
-    const initialPage = location.hash ? location.hash.substring(1) : "/index";
-    loadPage(initialPage, false);
-});
+//Escutar o evento click.
+btnCadEvento.addEventListener("click",function(impede){
+    //Impedir o comportamento padrão do link.
+    impede.preventDefault();
 
-//Cadastro Eventos
-/* exemplo de eventos
-let id = 1;
-let titulo = "Workshop de Git e Github";
-let tipo = "Workshop";
-let data = "2026-09-25";
-let local = "lab02";
-let descricao = "Atividade pratica sobre versionamento.";
-let status = "Agendado.";
-*/
-const Eventos = [
-    id,
-    titulo,
-    tipo,
-    data,
-    local,
-    descricao,
-    status
-]
+    btnDashboard.classList.remove("active");
+    btnCadEvento.classList.add("active");
+    btnCheckEvnt.classList.remove("active");
+
+    sDash[0].classList.add("d-none");
+    sNovo[0].classList.remove("d-none");
+    sEvnt[0].classList.add("d-none");
+})
+
+//Escutar o evento click.
+btnCheckEvnt.addEventListener("click",function(impede){
+    //Impedir o comportamento padrão do link.
+    impede.preventDefault();
+
+    btnDashboard.classList.remove("active");
+    btnCadEvento.classList.remove("active");
+    btnCheckEvnt.classList.add("active");
+
+    sDash[0].classList.add("d-none");
+    sNovo[0].classList.add("d-none");
+    sEvnt[0].classList.remove("d-none");
+})
+
+//Exibir o Dashboard ao abrir o sistema pela primeira vez.
+document.addEventListener("DOMContentLoaded",function(){
+    const navInicial = document.querySelector('[data-view="dashboard"]');
+    const secInicial = document.getElementsByClassName("view-dashboard");
+
+    navInicial.classList.add("active");
+    secInicial[0].classList.remove("d-none");
+})
+
+//==================================Cad d eventos==================================\\
+eventos = [
+    {
+        id:0,
+        titulo:"Corrida",
+        tipo:"Esporte",
+        data:"01/02/2003",
+        local:"Barra da Tijuca",
+        descricao:"O evento ocorrerá em comemoração ao aniversário da cidade.",
+        status:"Agendado"
+    },
+    {
+        id:1,
+        titulo:"Reunião dos fantoches",
+        tipo:"Workshop",
+        data:"04/05/2006",
+        local:"Maranhão",
+        descricao:"O evento ocorrerá para anunciar a abertura do clube de fantoches na escola de Raimundo F. Unido.",
+        status:"Agendado"
+    },
+    {
+        id:2,
+        titulo:"Festa da Uva",
+        tipo:"Festival",
+        data:"07/08/2009",
+        local:"São Tome dos Campos",
+        descricao:"O evento ocorrerá de acordo com a tradição anual da cidade.",
+        status:"Agendado"
+    }
+];
 
 //pega id dos inputs / botões
-const titulo = document.getElementById("titulo");
-const tipo = document.getElementById("tipo");
-const data = document.getElementById("data");
-const local = document.getElementById("local");
+const formulario= document.getElementById("form");
+const titulo    = document.getElementById("titulo");
+const tipo      = document.getElementById("tipo");
+const data      = document.getElementById("data");
+const local     = document.getElementById("local");
 const descricao = document.getElementById("descricao");
 
-//pega o id do botao
-const btnCad = document.getElementById("")
+//id criado fora do evento
+let id = 3;
 
 //cadastra dados
-function validateForm() {
-  const Eventos = document.forms[titulo][tipo][data][local][descricao].value;
-  if (x == "") {
-    alert("Area deve ser preenchida");
-    return false;
-  }
-  return "Evento cadastrado com sucesso!";
-}
+formulario.addEventListener("submit",function(){
+    //Ler os valores digitados com .value.
+    const nEvento = titulo.value;   //Remover espaços extras do título e do local usando trim().
+    const tEvento = tipo.value;
+    const dEvento = data.value;
+    const lEvento = local.value;    //Remover espaços extras do título e do local usando trim().
+    const dsEvent = descricao.value;
 
-//Dashboard
+    const neSemEspaco = nEvento.trim();
+    const leSemEspaco = lEvento.trim();
+
+    //Validar todos os campos obrigatórios.
+    if(nEvento == ""){
+        //Se houver campo vazio, exibir uma mensagem de erro na própria página.
+        alert("Falta preencher o nome");
+        return false;
+    }
+    if(tEvento == null){
+        alert("Falta preencher o tipo");
+        return false;
+    }
+    if(dEvento == null){
+        alert("Falta preencher a data");
+        return false;
+    }
+    if(lEvento == ""){
+        alert("Falta preencher o local");
+        return false;
+    }
+    if(dsEvent == ""){
+        alert("Falta preencher a descricao do evento");
+        return false;
+    }
+    
+    //Se estiver tudo correto, criar um novo objeto JavaScript.
+    const Evento    = new Object();
+    Evento.Id       = id++;         //Gerar um id que não repita o id dos eventos existentes.
+    Evento.titulo   = neSemEspaco;
+    Evento.tipo     = tEvento;
+    Evento.data     = dEvento;
+    Evento.local    = leSemEspaco;
+    Evento.descricao= dsEvent;
+    Evento.status   = "Agendado";   //O novo evento deve iniciar com status 'Agendado'.
+
+    //Adicionar o novo objeto ao array eventos.
+    eventos.push(Evento);
+
+    alert("Evento cadastrado com sucesso!");
+
+    formulario.reset()
+})
+
+//==================================Eventos==================================\\
+const barraPesquisa = document.getElementById("inputFiltroProduto");
+const cartoesEventos = document.getElementById("cartoes-eventos");
+
+//montar e mostrar cartoes
+/*
+document.addEventListener("DOMContentLoaded",function(){
+    const cartao = document.createElement("div");
+
+    for (let i = 0; eventos.length; i++) {
+
+    }
+    cartao.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${eventos[i].titulo}</h5>
+                <p class="card-text">${eventos[i.tipo]}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    `
+})
+*/
+
+barraPesquisa.addEventListener("input",function(){
+    //deixa o item do input tudo minusculo
+    const pesquisado = barraPesquisa.value.toLowerCase();
+
+    for (let i = 0; i < eventos.length; i++) {
+        //pegando os nomes dos eventos
+        const nomeEvento = eventos[i][titulo].innerText.toLowerCase();
+        
+        //se a barra estiver vazia
+        if (nomeEvento.includes(pesquisado)){
+            //mostra tudo
+            eventos[i].classList.remove("d-none");
+        //se tiver algo escrito, só mostra o q estiver escrito
+        } else {
+            eventos[i].classList.add("d-none");
+        }
+    }
+    
+})
